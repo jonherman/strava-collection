f1ddeefe5aa2f3965e3f52128cbc42da00df4e82
Jon Herman <jonherman@gmail.com>
2024-12-14 13:24:51 -0800

    Setup callback to fetch access token from Strava. I'm not sure this works but want to try this out on a differnt domain.

--- client/web/src/app/page.tsx
+++ client/web/src/app/page.tsx
@@ -2,13 +2,44 @@
 
 import { useSearchParams } from 'next/navigation';
 
+async function getAccessToken(clientId:string, clientSecret:string, code:string) {
+  const url = "https://www.strava.com/oauth/token";
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(
+        {
+          client_id: clientId,
+          client_secret: clientSecret,
+          code: code,
+          grant_type: 'authorization_code'
+        }
+      )
+    });
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 export default function Home() {
   const searchParams = useSearchParams();
   let code = searchParams.get('code');
   console.log("Authorization code is: " + code);
-  let clientId = '133314';
-  let redirectUrl = `http://localhost:3000?approval_prompt=force&scope=activity:write`;
+  const clientId = '133314';
+  const clientSecret = '76555058917134e613269637de2fb70f9a5ffa44';
+  let redirectUrl = `http://stravacollection.com:3000?approval_prompt=force&scope=activity:write`;
   let enableURL = `http://www.strava.com/oauth/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUrl}`;
+  if (code) {
+    getAccessToken(clientId, clientSecret, code);
+  }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -16,7 +47,6 @@ export default function Home() {
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href={enableURL}
-            target="_blank"
             rel="noopener noreferrer"
           >
             Enable Strava Collection for your Strava Account
