acab3d747e2abafc752b19bc9a313a55de931cf4
Jon Herman <jonherman@gmail.com>
2024-12-14 10:52:15 -0800

    Add hard-coded authorize URL toEnable Strava button.

--- client/web/src/app/page.tsx
+++ client/web/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href=""
+            href="http://www.strava.com/oauth/authorize?client_id=133314&response_type=code&redirect_uri=http://localhost:3000?approval_prompt=force&scope=activity:write"
             target="_blank"
             rel="noopener noreferrer"
           >
