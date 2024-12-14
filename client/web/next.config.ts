21fa5a00ead9d27adee1f88f6bd42e6872b99048
Jon Herman <jonherman@gmail.com>
2024-12-14 13:30:17 -0800

    disable suespense boundary for useSearchQueryParams

--- client/web/next.config.ts
+++ client/web/next.config.ts
@@ -5,4 +5,10 @@ const nextConfig: NextConfig = {
   reactStrictMode: false
 };
 
+module.exports = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+}
+
 export default nextConfig;
